00d0dae4856e0947a03df702a8e21cfe0513b236
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-02-17 15:59:53 -0500

    OngoingEffectAnimation does not need to live here.

--- src/ui/animations/index.ts
+++ src/ui/animations/index.ts
@@ -2,8 +2,6 @@ export * from './animation.interface';
 
 export * from './animator.interface';
 
-export * from './ongoing-effect-animation.interface';
-
 export * from './reaction-animation.interface';
 
 export * from './skill-animation.interface';
